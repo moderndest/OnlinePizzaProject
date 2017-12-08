bb1e21f5eea9f2a3a958058b57a510f8174d9d8d
modern <rattarasan.c@husky.neu.edu>
2017-12-08 02:10:48 -0500

    fix Header

--- js/LoadFile.js
+++ js/LoadFile.js
@@ -1,5 +1,9 @@
-<script>
-$(function(){
-    $("#header").load("header.html");
-  });
-  </script>
+// JavaScript Document
+
+
+$(document).ready(function(){
+	$("#header").load("header.html");
+});
+// $(function(){
+//	 $("#header").load("header.html");
+//   });
