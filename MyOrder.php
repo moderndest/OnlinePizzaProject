f446215ea7867e5597550917151bad6af0f6cb86
dalee212 <34355344+dalee212@users.noreply.github.com>
2017-12-12 19:06:10 -0500

    Myorder/WhereIsMyPizza
    add the function of searching from db and passing order number to the map page

--- MyOrder.php
+++ MyOrder.php
@@ -5,9 +5,31 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie-edge">  
         <link rel="stylesheet" href="css/style.css" />
+        <link rel="stylesheet" href="css/Checkout.css" />
+        <link rel="stylesheet" href="css/cart.css" />
+        <link rel="stylesheet" href="css/PaymentStyle.css" />
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"> </script>  
 		<script type="text/javascript" src="js/LoadFile.js"></script>   
         <title>My Order</title>
+
+        <style>
+        #ordertext {
+            font-size: 100%;
+            margin: 2%;
+            color: #f3a816;
+            text-align: left;
+            text-align: center;
+            margin-left: 10%;
+        }
+        .container2 {
+            height: 100%;
+            width: 80%;
+            min-width: 100px;
+                margin-left: 10%;
+            margin-right: 10%;
+        }
+        </style>
+
     </head>
     <body> 
             <!--  HEADER SECTION -->
@@ -23,13 +45,18 @@
         <script >onlinePizzaCart.displayCarttotal();</script>
 
         <div class="clearfix"></div>
-        <form method="POST">
+
+        <!-- <The form of searching order number, having action with CheckorderNum.php""> -->
+
+        <!-- <form action="CheckOrderNum.php" method="POST"> -->
+        <form action="WhereIsMyPizza.php" method="POST">
         <div class="myorderContent">
-            <img src"">
-            <input class"orderInput" type="text" required>
-            <button class"orderbutton" type="submit" formaction="SignUp/SignUp.php" formmethod="POST"> Enter </button>
+             <label><font color="f3a816">Please input order number<br></font></label>
+            <!-- <img src""> -->
+            <input class"orderInput" type="text" placeholder="Your order number" name="OrderNo" required style="width:auto;">            
+            <button class="loginButton" type="submit"  style="width:auto;">Enter</button>
         </div>
         </form>
-        
+        <div class="clearfix"></div>
     </body>
 </html>
--- WhereIsMyPizza.php
+++ WhereIsMyPizza.php
@@ -1,96 +1,139 @@
+<?php
+
+session_start();
+
+include ('Connectdb.php');
+
+//check get the order number from other page or not.
+if(empty($_POST['OrderNo'])){
+  echo "<script type='text/javascript'>window.location.href = 'MyOrder.php';</script>";  
+}      
+
+$orderno = $_POST['OrderNo'];
+
+// echo $orderno;
+$orderno = stripslashes($orderno);
+
+$query = "SELECT OrderNo FROM Orders WHERE OrderNo='$orderno'";
+
+// search the order no from the db
+$result = $dbh->prepare($query);
+$result->execute();
+$count = $result->rowCount();
+
+if($count != 1){
+    $message = "Incorrect OrderNo.";
+    echo "<script type='text/javascript'>alert('$message');window.location.href = 'MyOrder.php';</script>";
+}else
+{
+    // goto mapview
+}
+?>
+
 <!DOCTYPE html>
+
+
+
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie-edge">  
-        <link rel="stylesheet" href="css/style.css" />
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"> </script>  
-		<script type="text/javascript"  src="js/LoadFile.js"></script> 
-        <title>Where is My Pizza</title>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie-edge">  
+    <link rel="stylesheet" href="css/style.css" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"> </script>  
+<script type="text/javascript"  src="js/LoadFile.js"></script> 
+    <title>Where is My Pizza</title>
 
-        <style>
-            #map {
-              height: 300px;
-              width: 80%;
-              max-height: 80%;
+    <style>
+        #map {
+          height: 300px;
+          width: 80%;
+          max-height: 80%;
 
-              margin-left: 10%;
-              margin-right: 10%;
+          margin-left: 10%;
+          margin-right: 10%;
 
-             }
-             #ordertext {
-               font-size: 100%;
-               margin: 2%;
-               color: #f3a816;
-               text-align: left;
-               text-align: center;
-               margin-left: 10%;
-              }
-        </style>
+         }
+         #ordertext {
+           font-size: 100%;
+           margin: 2%;
+           color: #f3a816;
+           text-align: left;
+           text-align: center;
+           margin-left: 10%;
+          }
+    </style>
 
-        <!--animate-->
-        <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/animate.css/3.1.0/animate.min.css">
-        <link rel="stylesheet" type="text/css" href="css/animate.css" />
-        <!--jquery-->
-        <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>  
-        <!--waypoints-->
-        <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/waypoints/2.0.4/waypoints.min.js"></script>
-        <script src="js/waypoints.js" type="text/javascript"></script>
-        <!--fonts-->
-        <link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet">
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Tangerine"><!--pizza-->
-        <link href="https://fonts.googleapis.com/css?family=Timmana" rel="stylesheet">
-        <!--css-->
-        <link rel="stylesheet" type="text/css" href="css/style_menu.css"/>	
+    <!--animate-->
+    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/animate.css/3.1.0/animate.min.css">
+    <link rel="stylesheet" type="text/css" href="css/animate.css" />
+    <!--jquery-->
+    <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>  
+    <!--waypoints-->
+    <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/waypoints/2.0.4/waypoints.min.js"></script>
+    <script src="js/waypoints.js" type="text/javascript"></script>
+    <!--fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Tangerine"><!--pizza-->
+    <link href="https://fonts.googleapis.com/css?family=Timmana" rel="stylesheet">
+    <!--css-->
+    <link rel="stylesheet" type="text/css" href="css/style_menu.css"/>	
+    
+</head>
+<body> 
+        <!--  HEADER SECTION -->
+        <?php include 'header.php';?>
+        <!--Remaining section-->
+        <!-- Continue Coding here -->
         
-    </head>
-    <body> 
-            <!--  HEADER SECTION -->
-            <?php include 'header.php';?>
-            <!--Remaining section-->
-            <!-- Continue Coding here -->
-            <div id="ordertext"> Order NO.##123456 </br></div>
-            
-            <div id="map" align="center" valign="center"></div>
-            <script>
-              function initMap() {
-                var position = {lat: 42.339244, lng: -71.090835};
-                var map = new google.maps.Map(document.getElementById('map'), {
-                  zoom: 15,
-                  center: position
-                });  
+        
+        
+<!--            <div id="ordertext"> Order NO.##123456 </br></div>-->
 
-                var contentString = 'We Will be there in 10 minutes.';
-                var infowindow = new google.maps.InfoWindow({
-                  content: contentString
-                  
-                  });
-                var marker = new google.maps.Marker({
-                  position: position,
-                  map: map
-                });
-                marker.addListener('click', function() {
-                  infowindow.open(map, marker);
-                });
-                infowindow.open(map,marker);
-              }
-            </script>
+        <!-- Show the order number -->
+        <!-- session_start(); -->
+        <div id="ordertext"> Order No.
+            <?php $orderno = $_POST['OrderNo']; echo $orderno;?>
+        </div>
+    
+        <!-- Show the Google Map  -->
 
-            <script async defer
-            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD7L9U76qc4yzpJCBz1hSplChJxB7zkRlI&callback=initMap">
-            </script>
-            </div>
+        <div id="map" align="center" valign="center"></div>
+        <script>
+          function initMap() {
+            var position = {lat: 42.339244, lng: -71.090835};
+            var map = new google.maps.Map(document.getElementById('map'), {
+              zoom: 15,
+              center: position
+            });  
 
-            <div id="ordertext"> Driver Phone Number: 123-456-789 </br></div>
-            <div id="inner-wrapper">
-              <section class="one-third"></section>     
-              <section class="one-third"></section>
-            </div>  
-            <div class="clearfix"></div>
-        <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
-        <script src='http://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js'></script>
-        <script  src="js/CartFunction.js"></script> 
-        <script >onlinePizzaCart.displayCarttotal();</script>
-    </body>
+            var contentString = 'We Will be there in 10 minutes.';
+            var infowindow = new google.maps.InfoWindow({
+              content: contentString
+              
+              });
+            var marker = new google.maps.Marker({
+              position: position,
+              map: map
+            });
+            marker.addListener('click', function() {
+              infowindow.open(map, marker);
+            });
+            infowindow.open(map,marker);
+          }
+        </script>
+
+        <script async defer
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD7L9U76qc4yzpJCBz1hSplChJxB7zkRlI&callback=initMap">
+        </script>
+        </div>
+        <!-- Show the driver's number -->
+        <div id="ordertext"> Driver Phone Number: 123-456-789 </br></div>
+        <div id="inner-wrapper">
+          <section class="one-third"></section>     
+          <section class="one-third"></section>
+        </div>  
+        <div class="clearfix"></div>
+
+</body>
 </html>
